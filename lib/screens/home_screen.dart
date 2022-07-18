3f0b55e86cd27a9d774d24ef98c87dd061f67aa9
Wagner-Coche <wagnercoche@gmail.com>
2022-07-18 01:09:58 +0100

    create search bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,6 +12,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade200,
       appBar: AppBar(
         backgroundColor: Colors.deepOrangeAccent,
         leading: IconButton(
@@ -40,6 +41,53 @@ class _HomeScreenState extends State<HomeScreen> {
           )
         ],
       ),
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Column(
+          children: const <Widget>[
+            SizedBox(
+              child: Padding(
+                padding: EdgeInsets.only(
+                  top: 20,
+                  right: 25,
+                  left: 25
+                ),
+                child: TextField(
+                  cursorColor: Colors.deepOrangeAccent,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(vertical: 10),
+                    fillColor: Colors.white,
+                    filled: true,
+                    prefixIcon: Icon(Icons.search, color: Colors.grey, size: 20),
+                    suffixIcon: Icon(Icons.close, color: Colors.grey, size: 20),
+                    hintText: "Search Food or Restaurants",
+                    hintStyle: TextStyle(fontSize: 11.5),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(50)
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(50)
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(50)
+                      ),
+                      borderSide: BorderSide(
+                        color: Colors.deepOrangeAccent
+                      )
+                    )
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),      
     );
   }
 }
